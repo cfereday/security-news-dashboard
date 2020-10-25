cf415c17f05a37dc16dbe6665a10fecf59ea652c
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-10-25 16:51:13 +0100

    Urls of latest dev.to articles are shown
    - Uses react async hooks to help manage this

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 const justGetUrls = (allArticles) => {
     let allUrls = [];
@@ -16,9 +16,29 @@ const fetchLatestDevToNews = () => {
 
 
 export function App() {
-    const storyUrls = fetchLatestDevToNews().then((urls) => urls.toString().split(','));
-    console.log('storyUrls,', storyUrls);
-    return <div>
-        <p>hey</p>
-    </div>
+    const [articles, setArticles] = useState([]);
+    // do I need to change something
+
+    // could you...
+    useEffect(async () => {
+        // ...grab me the new articles
+        const newArticles = await fetchLatestDevToNews();
+        // ...and when done, save them to state
+        setArticles((oldArticles) => [...oldArticles, ...newArticles]);
+    }, []);
+    // rerun the ask if the value of this array changes
+
+    return (
+        // drawing the page
+            <div>
+                <p>hey</p>
+                {articles.map(article => {
+                return <a href={article}> cool </a>})}
+            </div>
+    )
 }
+
+/*
+render: when the state doesn't match what you see on screen - no longer in a static world!!!
+re-load: load page & stuff happens in background
+ */

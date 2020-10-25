b22a8bdd3054ca1fed21495983a8527fe41595b4
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-10-25 16:07:13 +0100

    Fetches latest 5 dev.to security articles
    - Currently a pending promise, need to figure out async with react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 
+const justGetUrls = (allArticles) => {
+    let allUrls = [];
+    for (const newsArticle of allArticles) {
+        allUrls.push(newsArticle.url);
+    }
+    return allUrls;
+};
+
+const fetchLatestDevToNews = () => {
+    return fetch('https://dev.to/api/articles?per_page=5&tag=security')
+        .then(response => response.json())
+        .then(latestArticles => justGetUrls(latestArticles))
+};
+
+
 export function App() {
+    const storyUrls = fetchLatestDevToNews().then((urls) => urls.toString().split(','));
+    console.log('storyUrls,', storyUrls);
     return <div>
-        <p>
-            Hey I am a dashboard!!
-        </p></div>
+        <p>hey</p>
+    </div>
 }

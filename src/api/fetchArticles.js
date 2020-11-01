a0b287c40821d5a022b9a30ed95cc23a887a0a16
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-11-01 18:43:11 +0100

    Latest 5 devTo articles are written to a file

--- src/api/fetchArticles.js
+++ src/api/fetchArticles.js
@@ -1,21 +1,26 @@
 import fetch from "node-fetch";
+import fs from "fs";
 
-const justGetUrls = (allArticles) => {
-    let allUrls = [];
-    for (const newsArticle of allArticles) {
-        allUrls.push(newsArticle.url);
-    }
-    return allUrls;
+const keyInfo = (allArticles) => {
+    return allArticles.map(({title, description, url}) => ({title, description, url}))
 };
 
 export const fetchLatestDevToNews = () => {
     return fetch('https://dev.to/api/articles?per_page=5&tag=security')
         .then(response => response.json())
-        .then(latestArticles => justGetUrls(latestArticles))
+        .then(latestArticles => keyInfo(latestArticles))
+};
+
+const createFile = (articles) => {
+    const flattenedArticles = articles.flat();
+    const latestArticles = flattenedArticles.map(JSON.stringify).join("\n");
+
+    fs.writeFile('devToArticles.txt', latestArticles, function (err) {
+        if (err) return console.log(err);
+    })
 };
 
 Promise.all([fetchLatestDevToNews()]).then((values) => {
-    console.log('******latest devTo news******');
-    console.log(values);
+    createFile(values);
 });
 

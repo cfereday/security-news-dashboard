7a6829e60179fd7b45ea271426a90c721344e1ae
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-11-02 09:33:30 +0100

    WIP Fetches Sec articles from Reddit
    - Promise.all approach doesn't seem to be working right - need to debug
    this

--- src/api/fetchArticles.js
+++ src/api/fetchArticles.js
@@ -1,17 +1,36 @@
 import fetch from "node-fetch";
 import fs from "fs";
 
-const keyInfo = (allArticles) => {
+const keyDevToInfo = (allArticles) => {
     return allArticles.map(({title, description, url}) => ({title, description, url}))
+};
+
+const keySubRedditInfo = (allArticles) => {
+    const subRedditArticles = allArticles.data.children.slice(0, 6);
+    const latestArticles = subRedditArticles.map(article => `### Article
+    Title
+    ${article.data.title} \n
+    Description
+    ${article.data.selftext}\n
+    Url
+    ${article.data.url}\n`).join("\n");
+
+    fs.writeFile('subRedditArtilces.md', latestArticles, function (err) {
+        if (err) return console.log(err);
+    })
 };
 
 export const fetchLatestDevToNews = () => {
     return fetch('https://dev.to/api/articles?per_page=5&tag=security')
         .then(response => response.json())
-        .then(latestArticles => keyInfo(latestArticles))
+        .then(latestArticles => keyDevToInfo(latestArticles))
 };
 
-
+const fetchLatestReddits = () => {
+    return fetch('https://www.reddit.com/r/security.json')
+        .then(response => response.json())
+        .then(latestArticles => keySubRedditInfo(latestArticles))
+};
 
 const createFile = (articles) => {
     const flattenedArticles = articles.flat();
@@ -28,7 +47,9 @@ const createFile = (articles) => {
     })
 };
 
-Promise.all([fetchLatestDevToNews()]).then((values) => {
-    createFile(values);
+Promise.all([fetchLatestDevToNews(), fetchLatestReddits()]).then((devToArticles, redditArticles) => {
+    createFile(devToArticles);
+    console.log('redditArticles', redditArticles)
+    keySubRedditInfo(redditArticles);
 });
 

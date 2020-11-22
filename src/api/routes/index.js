afc29e9cd35d54d9b021085d176f3bd0114b6b8d
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-11-22 11:21:19 +0100

    Adds express app
    This was built using the Express generator: https://expressjs.com/en/starter/generator.html

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET home page. */
 router.get('/', function(req, res, next) {
-    res.send('API is working properly');
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;

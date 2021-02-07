cb88d01a3808cc92116dcbb99431c3f338895dac
TBC <TBC>
2021-02-07 17:33:19 +0000

    Minor edits to improve code clarity

--- src/api/responseExtraction/npmAdvisories.js
+++ src/api/responseExtraction/npmAdvisories.js
@@ -1,18 +1,15 @@
 const fetch = require("node-fetch");
-const {Headers} = require("node-fetch");
+const { Headers } = require("node-fetch");
+
 const meta = {
     'X-Spiferack': '1',
     'Content-Type': 'application/json'
 };
 const headers = new Headers(meta);
 
-const extractOutAdvisories = ({advisoriesData: {objects}}) => {
-    const splicedAdvisories = objects.slice(0, 5);
-    return splicedAdvisories.map(advisory => {
-        const splitReferences = advisory.references.split('\n');
-        const resplitReferences = splitReferences.map(reference => {
-            return reference.substr(1);
-        });
+const extractOutAdvisories = ({ advisoriesData: { objects } }) => {
+    return objects.slice(0, 5).map(advisory => {
+        const resplitReferences = advisory.references.split('\n').map(reference => { return reference.substr(1) });
         return {
             title: advisory.title,
             description: 'The Overview: ' + advisory.overview + ' Recommendations: ' + advisory.recommendation,
@@ -22,7 +19,7 @@ const extractOutAdvisories = ({advisoriesData: {objects}}) => {
 };
 
 const fetchLatestAdvisories = async () => {
-    return await fetch('https://www.npmjs.com/advisories/', {headers})
+    return await fetch('https://www.npmjs.com/advisories/', { headers })
         .then(response => response.json())
         .then(latestAdvisories => extractOutAdvisories(latestAdvisories))
 };
